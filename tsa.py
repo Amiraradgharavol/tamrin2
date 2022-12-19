9ac3b992e90c991297b50b6f9118f61fff20a8ad
Amiraradgharavol <120362985+Amiraradgharavol@users.noreply.github.com>
2022-12-19 19:47:06 +0330

    Add files via upload

---
+++ tsa.py
@@ -0,0 +1,12 @@
+import random
+tas = random.randint(1, 6)
+prize = random.randint(1, 6)
+while True:
+    if tas != 6:
+        print(tas,"bakhti")
+        break
+    elif  tas == 6:
+        print("tas number", tas)
+        print("barande shodi va in tas avalet beonvan jaieze ", prize)
+        print("tas dovomet beonvan jaieze", prize)
+        break
